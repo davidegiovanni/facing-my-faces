fd60c8d6e9d252531c6d5aa6bab5cc422409a64d
Davide Giovanni Steccanella <33760151+davidegiovanni@users.noreply.github.com>
2022-09-08 12:21:03 +0200

    style: face page layout

--- app/routes/$lang/faces/$face/index.tsx
+++ app/routes/$lang/faces/$face/index.tsx
@@ -120,8 +120,9 @@ export default function ItemPage() {
       <div className="lg:hidden -mt-20 overflow-hidden relative z-50">
         <img src="/icons/divider-hr.png" className="w-full scale-x-150" alt="" />
       </div>
-      <div className="relative z-10 -mt-8 lg:mt-0 h-full flex items-center justify-center text-center lg:overflow-y-auto pb-32 lg:pb-0">
+      <div className="relative z-10 -mt-8 lg:mt-0 h-full flex items-start justify-center text-center lg:overflow-y-auto pb-32 lg:pb-0">
         <div className="w-11/12 lg:w-9/12 max-w-screen-xl mx-auto mb-4">
+          <div className="h-[20vh] w-full hidden lg:block" />
         <div className="w-1/2 mix-blend-multiply lg:w-1/2 h-full max-w-screen-md mx-auto">
             <Attachment attachment={{
               id: "",
